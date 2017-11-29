fb37b824d27cd145825b2d650f49801724197218
sfrjrs <json.spence@gmail.com>
2017-11-28 22:27:28 -0800

    adding header and footer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'JsonFiles';
+
+  // Properties
+  private title : string;
+
+  // Init
+  public constructor() {
+    this.updateSiteName('jsonFiles');
+    this.title = this.getSiteName();
+  }
+
+  // Get | Return site title
+  public getSiteName() : string
+  {
+    return this.title;
+  }
+
+  // Set | Update site title
+  public updateSiteName(newName : string)
+  {
+    this.title = newName;
+  }
+
 }
